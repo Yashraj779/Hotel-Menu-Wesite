4ca9ab3c99aac643ea232577e493a4e9687fa2bc
Yashraj779 <167800636+Yashraj779@users.noreply.github.com>
2024-09-21 10:43:46 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function filterMenu(category) {
+    const menuItems = document.querySelectorAll('.menu-item');
+
+    menuItems.forEach(item => {
+        if (category === 'all') {
+            item.style.display = 'block';
+        } else {
+            if (item.classList.contains(category)) {
+                item.style.display = 'block';
+            } else {
+                item.style.display = 'none';
+            }
+        }
+    });
+}
